7bc55d907f4a6b124a16e044c41e4006103546b9
Huynh Lan <huynhlan.fe21@gmail.com>
2025-01-15 10:16:26 +0700

    setup project with next.js

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  webpack: (cfg) => {
+    cfg.module.rules.push({
+      test: /\.md$/,
+      loader: "frontmatter-markdown-loader",
+      options: { mode: ["react-component"] },
+    });
+    return cfg;
+  },
+};
+
+export default nextConfig;
